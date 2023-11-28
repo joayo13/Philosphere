a3a543f3fc908b5d4da5da81ecd3f55a9f43b3cd
Jordan Ayotte <jordanayotte13@outlook.com>
2023-11-28 14:18:29 -0500

    added rest of fields for user model, and now includes password encryption with bcryptjs

--- models/user.js
+++ models/user.js
@@ -1,9 +1,45 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: {type: String}
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 16,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  quotes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Quote',
+  }],
+});
+
+UserSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 // Virtual for author's URL
