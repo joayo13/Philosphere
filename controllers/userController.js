d82f9477d280f98efa1f6cb355c46784fa4fe033
Jordan Ayotte <jordanayotte13@outlook.com>
2023-12-19 01:32:24 -0500

    displays story on page with styles saved from entry

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,16 @@
+const Story = require("../models/story");
 const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Authors.
 exports.user_get = asyncHandler(async (req, res, next) => {
   const userData = await User.findOne({_id: req.params.id}).exec();
+  const userStoryData = await Story.find({author: req.params.id}).exec()
   if(userData) {
     console.log(req.user)
     res.render('user-page', {
-      username: userData.username
+      username: userData.username,
+      storyData: userStoryData
     })
   }
     else {
